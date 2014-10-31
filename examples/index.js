ada5b699d6d13d006a95d77e9623c52c8817004b
kgryte <kgryte@gmail.com>
2014-10-31 11:11:30 -0700

    [UPDATE] lib, examples. WIP.

--- examples/index.js
+++ examples/index.js
@@ -1,1 +1,15 @@
-var module = require( './../lib' );
+'use strict';
+
+var unique = require( './../lib' );
+
+// Simulate some data...
+var data = new Array( 1000 );
+
+for ( var i = 0; i < data.length; i++ ) {
+	data[ i ] = Math.round( Math.random()*10 );
+}
+
+// Determine the unique values:
+unique( data );
+
+console.log( data.join( '\n' ) );
